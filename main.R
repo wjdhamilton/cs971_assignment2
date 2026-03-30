a292720d99f38b6f7c891975adb2b90956a78d4e
James Hamilton <01-repose-alive@icloud.com>
2026-03-30 20:02:05 +0100

    removed conflict

--- main.R
+++ main.R
@@ -105,11 +105,7 @@ forecastingfitnessRMSE <- function(expr) {
   if (any(is.nan(result)))
     Inf
   else
-<<<<<<< HEAD
-    sqrt(mean((TrainingData$BRK.B.Close - result)^2))
-=======
     sqrt(mean((TrainingData[, volume] - result)^2))
->>>>>>> risk_engine
 }
 
 # Helper function that returns a scalar from the cor function. Cannot be used 
