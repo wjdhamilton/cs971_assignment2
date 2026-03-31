860470667eb1f60b34b146ea150deb83550dde5b
James Hamilton <01-repose-alive@icloud.com>
2026-03-31 19:51:57 +0100

    algorithm wrapped and martingales discussed

--- main.R
+++ main.R
@@ -15,15 +15,23 @@ library(BatchGetSymbols)
 data_start <- "2000-01-01"
 data_end   <- "2026-01-01"
 
-tkr  <- "AAPL"
 
-run_GP <- \(grammar, fit_fun, min_lag, max_lag, data_start, data_end, tkr) {
+
+run_GP <- \(grammar,
+            fit_fun,
+            min_lag,
+            max_lag,
+            data_start,
+            data_end,
+            tkr,
+            pre_processor  = identity,
+            post_processor = identity) {
+
   # Download into a named list (preserves order)
   FullDataXTS <- getSymbols(tkr, src = "yahoo",
                             from = data_start,
                             to   = data_end,
-                            auto.assign = FALSE)
-
+                            auto.assign = FALSE) |> pre_processor()
 
   ########## Change '-' in ticker names to '.' because R has a delightful habit of
   ########## unexpectedly sanitising the column names so they can be used as symbols
@@ -45,7 +53,6 @@ run_GP <- \(grammar, fit_fun, min_lag, max_lag, data_start, data_end, tkr) {
   var_names <- character(0)
 
   lagged_data <- FullDataXTS
-  lagged_data[, volume] <- log(lagged_data[, volume]) # Scale the volume to a more amenable number
 
   for (f in fields) {
     for (i in min_lag:max_lag) {
@@ -58,7 +65,6 @@ run_GP <- \(grammar, fit_fun, min_lag, max_lag, data_start, data_end, tkr) {
   }
 
   lagged_data <- na.omit(lagged_data)
-
   halfway <- mean(as.Date(c(data_start, data_end)))
   training_end <- paste0("/", halfway)
   testing_start <- paste0(as.Date(halfway) + 1, "/")
@@ -71,7 +77,6 @@ run_GP <- \(grammar, fit_fun, min_lag, max_lag, data_start, data_end, tkr) {
 
   # This makes the Lag_1, Lag_2 etc. argument find its way to the TrainingData frame
   env <- list2env(as.list(TrainingData), parent = .GlobalEnv)
-
   grammar$data <- do.call(grule, lapply(var_names, as.name))
 
   ForecastingGrammar <- CreateGrammar(grammar)
@@ -81,20 +86,16 @@ run_GP <- \(grammar, fit_fun, min_lag, max_lag, data_start, data_end, tkr) {
                              \(expr) fit_fun(env, TrainingData[, close], expr),
                              terminationCost = 0.05,
                              max.depth = 5)
+
   # Evaluation
-  ForecastingModel <- ge$best$expressions[1]
-  eval(ForecastingModel, envir = env)
+  ForecastingModel  <- ge$best$expressions[1]
+  # eval(ForecastingModel, envir = env)
+  pred_test         <- with(TestingData, eval(ForecastingModel))
+  cls               <- TestingData[, close]
+  comparator        <- merge(cls, prediction = pred_test) |> post_processor()
+  rmse_test         <- sqrt(mean((cls - pred_test)^2)) |> post_processor()
 
-  pred_test <- with(TestingData, eval(ForecastingModel))
-  # pred_test <- lag.xts(pred_test, k = -1)
-
-  cls <- TestingData[, close]
-
-  comparator <- merge(cls, prediction = pred_test)
-
-  rmse_test <- sqrt(mean((cls - pred_test)^2))
-
-  print(tail(comparator, 10))
+  print(tail(comparator, 5))
   print(ForecastingModel)
   print(rmse_test)
 }
@@ -116,7 +117,7 @@ p_log <- \(num) {
 }
 
 p_sin <- \(num) {
-  if(any(!is.finite(num))) {
+  if (any(!is.finite(num))) {
     0
   } else {
     sin(num)
@@ -124,28 +125,27 @@ p_sin <- \(num) {
 }
 
 p_cos <- \(num) {
-  if(any(!is.finite(num))) {
+  if (any(!is.finite(num))) {
     0
   } else {
     cos(num)
   }
-  }
+}
 
 
 
-  # Grammar
+# Grammar - note that the data rule, rhich references the columns of the lagged
+# dataset is introduced during processing
 ForecastingRules <- list(expr       = grule(op(arithmetic, arithmetic),
                                             reducer(lists),
                                             func(arithmetic),
-                                            data),
+                                            data
+                                            ),
                          arithmetic = grule(op(arithmetic, arithmetic), # Things that are acceptable to an arithmetic function in R
                                             func(arithmetic),
                                             reducer(lists),
-                                            # stats(lists),  Don't do this, returns a correlation matrix
                                             stats(data),
-                                            #stats2(lists, lists), Don't do this since cor(list, list) returns a correlation matrix
                                             stats2(data, data),
-                                            data,
                                             const),
                          func       = grule(p_sin, p_cos, exp, p_log),
                          op         = grule('+', '-', '*', '/'),
@@ -159,14 +159,32 @@ ForecastingRules <- list(expr       = grule(op(arithmetic, arithmetic),
 # Fitness function (RMSE)
 fitness <- forecastingfitnessRMSE <- function(env, training, expr) {
   result <- eval(expr, envir = env)
-  if (any(is.nan(result)))
+  # Get rid of NaNs, and sometimes the expression returns a single value
+  if (any(is.nan(result)) || length(result) != length(training)) 
     Inf
   else
     sqrt(mean((training - result)^2))
 }
 
 # operating DF
-max_lag <- 21 # 1 month of trading
-min_lag <- 5
+max_lag <- 200 # 1 month of trading
+min_lag <- 1
 
-run_GP(ForecastingRules, fitness, min_lag, max_lag, data_start, data_end, tkr)
+log_processor       <- \(data) log(data)
+log_diff_processor  <- \(data) data |> log() |> diff() |> na.omit()
+
+log_post_processor      <- \(data) exp(data)
+log_diff_post_processor <- \(data) data |> cumsum() |> exp()
+
+tkr  <- "KGF.L"
+
+run_GP(ForecastingRules,
+       fitness,
+       min_lag,
+       max_lag,
+       data_start,
+       data_end,
+       tkr,
+       pre_processor  = log_processor,
+       post_processor = log_post_processor)
+
