d027271584d700f414924887023a609a2f8358fb
James Hamilton <01-repose-alive@icloud.com>
2026-04-06 14:06:02 +0100

    with the changes

--- trading_system.R
+++ trading_system.R
@@ -13,11 +13,15 @@ past_data     <- data_set[1]
 
 # Construct a trade object. The close_price should never be empty on a 
 # closed trade; close_price == NULL represents a program failure
-mk_trade <- \(direction, open_price) {
-  list(direction = direction, open = open_price, close_price = NULL)
+mk_trade <- \(direction, open_price, fee) {
+  list(direction = direction,
+       open = open_price,
+       close_price = NULL,
+       fee = fee
+  )
 }
 
-open_position <- mk_trade(direction = 0, open_price = 0)
+open_position <- mk_trade(direction = 0, open_price = 0, fee = 0)
 
 for (i in 2:NROW(data_set)) {
   # Any real-world strategy must generate a signal using yesterday's data
@@ -26,30 +30,40 @@ for (i in 2:NROW(data_set)) {
            error = function(e) {
              if (grepl("outside valid range", e$message)) {
                NULL
-             } else {
-               # have a fit
+             } else { # have a fit
                stop(e)
              }
            }
   )
+
   # Signals on early data are likely to produce NA since the strategy will probably
   # use moving averages. Skip these, but keep adding to past_data
   latest_data <- data_set[i]
-  # Add today's close to the past_data series
+  # Add today's ohlcv to the past_data series
   past_data <-rbind.xts(past_data,latest_data)
-  if (is.null(model)) { next }
+  # Get the last prices as a number
+  latest_close <- as.numeric(latest_data[, close])
+  latest_open  <- as.numeric(latest_data[, open])
+
+  if (is.null(model)) { next } # Skip nulls from the tryCatch
+
   # Latest signal is the last signal in the model
   signal <- as.numeric(last(model))
   # Do some trading
   current_direction <- open_position$direction
-  latest_close <- as.numeric(latest_data[, close])
+
   if(current_direction != signal){
     #Close the current position
     open_position$close_price <- latest_close
     closed_trades <- c(closed_trades, list(open_position))
-    # Open a new position
-    open_position <- mk_trade(direction = signal, open_price = latest_close)
+    # Open a new position, assuming a trading fee of 2% per trade. 
+    assumed_fee <- 0.02
+    open_position <- mk_trade(direction   = signal,
+                              open_price  = latest_close,
+                              fee         = assumed_fee
+    )
   } 
+
   # If we've reached the end of the dataset, just close the final trade
   if(i == NROW(data_set)) {
     open_position$close_price <- latest_close
@@ -57,7 +71,7 @@ for (i in 2:NROW(data_set)) {
   }
 }
 
-trade_profit <- \(trade) trade$direction * (trade$close_price - trade$open)
+trade_profit <- \(trade) (trade$direction * (trade$close_price - trade$open) - trade$fee)
 
 total_profit <- lapply(closed_trades, trade_profit) |> as.numeric() |> sum()
 
